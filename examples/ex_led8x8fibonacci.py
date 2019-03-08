81338314ab06ec94f8109422b7e980e6c579e4b1
Dave Wilson <parttimehacker@gmail.com>
2019-03-08 06:53:27 -0800

    Update ex_led8x8fibonacci.py
    Changed imports

--- examples/ex_led8x8fibonacci.py
+++ examples/ex_led8x8fibonacci.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 """ Example of the Fibonacci 8x8 LED matrix dispplay class """
 
-import time
-from threading import Thread, Lock
+import threading
 
 from Adafruit_Python_LED_Backpack.Adafruit_LED_Backpack import BicolorMatrix8x8
 
 import led8x8fibonacci
 
 if __name__ == '__main__':
-    LOCK = Lock()
+    LOCK = threading.Lock.Lock()
     DISPLAY = BicolorMatrix8x8()
-    FIB = led8x8fibonacci.LedMatrix8x8(MATRIX, LOCK)
+    FIB = led8x8fibonacci.Led8x8Fibonacci(MATRIX, LOCK)
     FIB.reset()
     while True:
     	FIB.display()
